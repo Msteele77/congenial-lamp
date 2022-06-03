ac7f807e9e6e05c1327b5b991d5688575f748ab5
Macheya Steele <mcheysteele@gmail.com>
2022-06-02 21:00:45 -0600

    added readme, adds outline of js

---
+++ README.md
@@ -0,0 +1,7 @@
+# project to demonstrate bored api
+
+## Technologies Used:
+
+### Follow link to explore the application
+
+### Application Preview
---
+++ script.js
@@ -0,0 +1,18 @@
+//funciton to handle display of generator results
+var endPoint = `https://www.boredapi.com/api/activity`;
+var submitButton = document.querySelector('.button');
+
+var displayResults = function () {
+
+
+}
+
+var seachHandler = function () {
+
+
+}
+
+
+
+
+//submitButton.addEventListener("click", searchHandler);
